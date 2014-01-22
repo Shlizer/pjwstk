b52fe7cfcff1df81d37b4537415137c18c7900f3
Shlizer <shlizer@wp.pl>
2014-01-22 17:09:24 +0100

    dummy foldery

--- src/main/java/pl/ayz/shlizer/pjwstk/main/Main.java
+++ src/main/java/pl/ayz/shlizer/pjwstk/main/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	public static void main(String[] args) {
 			MysqlUOW uow = new MysqlUOW();
 			MysqlStudent dao = new MysqlStudent(uow);
+			
 			System.out.println("koniec");
 	}
 
